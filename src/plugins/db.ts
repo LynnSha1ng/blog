b2356907235a59fe4cf0f83fec5145d50db864a7
LynnSha1ng <xiawang0441@gmail.com>
2024-10-14 19:56:34 +0800

    declare DB for post data

---
+++ src/plugins/db.ts
@@ -0,0 +1,32 @@
+import type { EntityTable, Table } from 'dexie';
+
+import Dexie from 'dexie';
+
+const db = new Dexie('Blog') as Dexie & {
+  post: EntityTable<Blog.Post.DBItem, 'name'>;
+  pagination: Table<Blog.Pagination, [string, number]>;
+};
+
+db.version(1).stores({
+  post: 'name, category, *tag, birthTime, metaVer',
+  pagination: '[mark+index], firstEntry, ver',
+});
+
+export { db };
+
+/* 清空IndexedDB，调试用
+
+try {
+  const DBs = await indexedDB.databases();
+  for (const db of DBs) {
+    const name = db.name;
+    if (name) {
+      indexedDB.deleteDatabase(name);
+      console.log(`已删除数据库: ${name}`);
+    }
+  }
+} catch (err) {
+  console.error(err);
+}
+
+*/
