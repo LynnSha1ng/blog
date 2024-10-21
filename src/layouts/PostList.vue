14f73c6876f0fbe521a60604419daf04242550be
LynnSha1ng <xiawang0441@gmail.com>
2024-10-21 20:46:04 +0800

    add pagination btns and currentPage state persistence in sessionStorage

--- src/layouts/PostList.vue
+++ src/layouts/PostList.vue
@@ -13,14 +13,32 @@
         v-for="{ metaVer, contVer, cont, ...item } in data"
         :key="`post-${item.name}`" />
     </ul>
-    <button @click="handleClick">下一页</button>
+    <div class="pagination">
+      <PaginationButton
+        icon-class="icon-arrow-double-left"
+        :disabled="isFirstPage"
+        @click="currentPage = 1" />
+      <PaginationButton
+        icon-class="icon-arrow-left-bold"
+        :disabled="isFirstPage"
+        @click="prev" />
+      {{ currentPage }}
+      <PaginationButton
+        icon-class="icon-arrow-right-bold"
+        :disabled="isLastPage"
+        @click="next" />
+      <PaginationButton
+        icon-class="icon-arrow-double-right"
+        :disabled="isLastPage"
+        @click="currentPage = pageCount" />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="tsx">
 /* global Blog */
-import type { ComputedRef, MaybeRefOrGetter } from 'vue';
-import { type ResponseWithCancelFn } from '@/utils/tool';
+import type { ComputedRef, MaybeRefOrGetter, FunctionalComponent } from 'vue';
+import type { ResponseWithCancelFn } from '@/utils/tool';
 
 const { withCriterion, onPageChange } = defineProps<{
   itemStyle: 'detailed' | 'brief';
@@ -31,9 +49,10 @@ const { withCriterion, onPageChange } = defineProps<{
 import PostListItem from '@/components/PostListItem.vue';
 
 import { ref, computed, watchEffect, toValue, onWatcherCleanup } from 'vue';
-import { useRoute } from 'vue-router';
-import { useOffsetPagination } from '@vueuse/core';
+import { useRoute, onBeforeRouteLeave } from 'vue-router';
+import { useOffsetPagination, useSessionStorage } from '@vueuse/core';
 import { fetchStat } from '@/api';
+import { sessionStorageUtils } from '@/utils/tool';
 
 const route = useRoute();
 const criterion = (() => {
@@ -47,7 +66,7 @@ const stat = await fetchStat();
 let currentTotal: MaybeRefOrGetter<number>;
 let currentCriterionName: ComputedRef<string>;
 let listTitle: string;
-let fetchData: (params: {
+let _fetchWrapperFn: (params: {
   index: number;
   total: number;
   criterionName: string;
@@ -66,42 +85,78 @@ if (withCriterion) {
 
   currentTotal = computed(() => stat[criterion!][currentCriterionName.value]);
 
-  fetchData = ({ index, total, criterionName }) =>
+  _fetchWrapperFn = ({ index, total, criterionName }) =>
     onPageChange(criterionName, index, total);
 } else {
   currentTotal = stat.total.post ?? 0;
 
-  fetchData = ({ index, total }) => onPageChange(index, total);
+  _fetchWrapperFn = ({ index, total }) => onPageChange(index, total);
 }
 
-const _fetchPage = async (index: number) => {
-  data.value = void 0;
-  const { response, cancel } = fetchData({
+const fetchPage = async (index: number, init = false) => {
+  const { response, cancel } = _fetchWrapperFn({
     index,
     total: toValue(currentTotal),
     criterionName: toValue(currentCriterionName),
   });
-  onWatcherCleanup(cancel);
+  if (!init) onWatcherCleanup(cancel);
   const pageData = await response;
   if (pageData) data.value = pageData;
 };
 
 const data = ref<Blog.Post.DBItem[]>();
+const indexRecord = useSessionStorage('lastPageIndex', 1, {
+  mergeDefaults: true,
+});
+
+const { currentPage, pageCount, isFirstPage, isLastPage, prev, next } =
+  useOffsetPagination({
+    total: currentTotal,
+    page: indexRecord,
+    pageSize: parseInt(import.meta.env.VITE_LIST_LIMIT),
+    onPageChange: async ({ currentPage }) => {
+      await fetchPage(currentPage);
+      indexRecord.value = currentPage;
+    },
+  });
 watchEffect(async () => {
-  await _fetchPage(1);
+  await fetchPage(toValue(indexRecord));
+});
+onBeforeRouteLeave(to => {
+  if (to.name !== ':postName') {
+    sessionStorageUtils.setItem('lastPageIndex', 1);
+  }
 });
 
-const { currentPage, pageCount } = useOffsetPagination({
-  total: currentTotal,
-  pageSize: parseInt(import.meta.env.VITE_LIST_LIMIT),
-  onPageChange: async ({ currentPage }) => {
-    await _fetchPage(currentPage);
-  },
-});
-
-const handleClick = () => {
-  currentPage.value = (currentPage.value % pageCount.value) + 1;
+const PaginationButton: FunctionalComponent<
+  { iconClass: string; disabled: boolean; label?: string },
+  { click(): void }
+> = (props, { emit }) => {
+  return (
+    <button
+      class={[
+        'pagination-btn',
+        'iconfont',
+        props.iconClass,
+        props.disabled ? '--disabled' : '',
+      ]}
+      onClick={() => emit('click')}>
+      {props.label}
+    </button>
+  );
 };
+PaginationButton.props = {
+  iconClass: {
+    type: String,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    required: true,
+  },
+  label: String,
+};
+PaginationButton.emits = ['click'];
 </script>
 
 <style lang="scss">
@@ -120,6 +175,32 @@ const handleClick = () => {
     @include screenBelow($sm) {
       grid-template-columns: 1fr;
     }
+  }
+}
+
+.pagination {
+  @include flex(center, center, wrap);
+  gap: 8px;
+  margin: 12px 0;
+}
+
+.pagination-btn {
+  padding: 12px;
+  border-radius: 12px;
+  background-color: var(--bg-3);
+  color: var(--text-regular);
+  transition-property: background-color, color;
+  transition-duration: 0.35s;
+
+  &.--disabled {
+    background-color: var(--bg-1);
+    border: 2px solid var(--bg-2);
+    color: var(--text-grey);
+    cursor: not-allowed;
+  }
+
+  &:not(.--disabled):hover {
+    background-color: var(--bg-4);
   }
 }
 </style>
