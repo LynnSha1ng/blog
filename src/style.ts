d36c0197b671077a223d2d0465d85c07d380690d
LynnSha1ng <xiawang0441@gmail.com>
2024-10-21 14:08:16 +0800

    use local dev iconfont instead of Alicdn

--- src/style.ts
+++ src/style.ts
@@ -1,5 +1,6 @@
 import '@/assets/styles/theme.css';
 import '@/assets/styles/base.css';
+import '@/assets/styles/iconfont.css';
 
 import '@/assets/styles/common.scss';
 import '@/assets/styles/transition.scss';
