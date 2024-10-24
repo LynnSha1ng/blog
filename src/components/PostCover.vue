579aea87fc40aaf5d29ed542036cfa1176fab913
LynnSha1ng <xiawang0441@gmail.com>
2024-10-24 18:53:23 +0800

    WIP: post cover

--- src/components/PostCover.vue
+++ src/components/PostCover.vue
@@ -5,29 +5,47 @@
       '--shown': atPostPage,
     }">
     <img class="cover-img" :src="postInfo.coverUrl" />
-    <header class="post-info">
+
+    <div class="banner-mask"></div>
+
+    <header class="post-info" v-show="ready">
       <h1 class="post-title">{{ postInfo.title }}</h1>
-      <div class="post-meta" v-show="ready">
-        <span class="publish-time">
-          <i class="meta-icon iconfont icon-caidan"></i>
-          <span class="time-txt">发表于</span>
-          <time :datetime="postInfo.birthTime">{{
-            formatDate(postInfo.birthTime)
-          }}</time>
-        </span>
-        <span class="last-modify-time">
-          <i class="meta-icon iconfont icon-caidan"></i>
-          <span class="time-txt">最后修改于</span>
-          <time :datetime="postInfo.mTime">
-            {{ formatDate(postInfo.mTime) }}
-          </time>
-        </span>
+
+      <div class="post-meta">
+        <div class="date-related">
+          <span class="publish-time">
+            <i class="meta-icon iconfont icon-calendar"></i>
+            <time class="meta-txt" :datetime="postInfo.birthTime">
+              {{ publishTimeTip }}
+            </time>
+          </span>
+
+          <span class="last-modify-time">
+            <i class="meta-icon iconfont icon-edit"></i>
+            <time class="meta-txt" :datetime="postInfo.birthTime">
+              {{ mTimeTip }}
+            </time>
+          </span>
+        </div>
+
+        <div class="cont-related">
+          <span class="post-length">
+            <i class="meta-icon iconfont icon-text"></i>
+            <span class="meta-txt">共字</span>
+          </span>
+
+          <span class="read-time">
+            <i class="meta-icon iconfont icon-time"></i>
+            <span class="meta-txt">读完大约需要</span>
+          </span>
+        </div>
       </div>
     </header>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import { usePostStore } from '@/stores';
 import { formatDate } from '@/utils/tool';
@@ -35,6 +53,19 @@ import { formatDate } from '@/utils/tool';
 const postStore = usePostStore();
 const { postInfo } = postStore;
 const { ready, atPostPage } = storeToRefs(postStore);
+
+const publishTimeTip = computed(() => {
+  if (postInfo.birthTime !== '') {
+    return `发表于${formatDate(postInfo.birthTime)}`;
+  }
+  return void 0;
+});
+const mTimeTip = computed(() => {
+  if (postInfo.mTime !== '') {
+    return `最后修改于${formatDate(postInfo.mTime)}`;
+  }
+  return void 0;
+});
 </script>
 
 <style lang="scss" scoped>
@@ -43,9 +74,7 @@ const { ready, atPostPage } = storeToRefs(postStore);
   overflow: hidden;
   opacity: 0;
   height: 0;
-  transition-property: height, opacity;
-  transition-duration: 0.45s;
-  transition-delay: -0.1s;
+  @include transition((height, opacity), 0.45s, -0.1s);
   will-change: height, opacity;
 
   &.--shown {
@@ -53,19 +82,42 @@ const { ready, atPostPage } = storeToRefs(postStore);
     height: 400px;
   }
 }
+
 .cover-img {
   object-fit: cover;
+}
+
+.banner-mask {
+  position: absolute;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.15);
+
+  @include onDarkMode {
+    background-color: rgba(0, 0, 0, 0.35);
+  }
 }
 
 .post-info {
   @include flex(center, center, column);
   position: absolute;
   inset: 0;
-  background-color: rgba(0, 0, 0, 0.25);
+  color: var(--text-white);
 }
 
 .post-meta {
   margin-block-start: 1em;
-  @include inline-separator('|');
+  text-align: center;
+
+  > * {
+    @include inline-separator('|', 8px, var(--text-white));
+  }
+}
+
+.meta-icon {
+  margin-inline-end: 4px;
+}
+
+.meta-txt {
+  font-size: 1rem;
 }
 </style>
